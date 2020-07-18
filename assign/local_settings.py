5e705ddc7e81fb4e56e74246a4517c441219a535
Prernaswarna <prernaswarnakumar@gmail.com>
2020-07-18 17:47:59 +0530

    catch

---
+++ assign/local_settings.py
@@ -0,0 +1,6 @@
+database_user='guest'
+database_password='Guest123'
+email_username = 'bugfixapp@gmail.com'
+email_password = 'dummy123'
+my_client_secret='TgORjftOyZ0zikE5icUEUw22Q1RP3W3tRckzmZsIjBe2W9HegNsU71X9Ps1H8E6N6ZRu4cFhZqUnPf9MKAT8d5Kar9Drp8apcfNHyumFoR38n3JNSPahVmjv1cSmcGw'
+
