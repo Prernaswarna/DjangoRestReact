a1df5cb1985e750e5581d63c17a49cbad999bb22
Prernaswarna <prernaswarnakumar@gmail.com>
2020-07-18 18:02:45 +0530

    Template

---
+++ assign/local_settings_template.py
@@ -0,0 +1,7 @@
+database_user='your databse username'
+database_password='your database password'
+email_username = 'your email id'
+email_password = 'your email password'
+my_client_secret='your client secret '
+
+
